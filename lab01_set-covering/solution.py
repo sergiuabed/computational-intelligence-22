4f07387cf595ce917c88635809f3ccf3539662f6
Sergiu Abed s295149 <s295149@studenti.polito.it>
2022-10-16 16:37:37 +0200

    First working version with breadth first search

---
+++ lab01_set-covering/solution.py
@@ -0,0 +1,38 @@
+from gx_utils import *
+from utilities import *
+import random
+
+def problem(N, seed=None):
+    random.seed(seed)
+    return [
+        list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
+        for n in range(random.randint(N, N * 5))
+    ]
+if __name__ == '__main__':
+    seed = 42
+    N = 10
+
+    l = problem(N, seed)
+    initialState = State({})
+
+    state_cost = dict()
+    parent_state = dict()
+    goal_test = lambda s: s == State(set(range(0, N)))
+
+    print(len(l))
+    print()
+    print()
+
+    #path = search(initialState, set(range(0, 5)), dict(), dict(), lambda x : len(state_cost))
+    path, cost = search(initialState, goal_test = goal_test, parent_state = parent_state, state_cost = state_cost, priority_function = lambda s : len(state_cost), unit_cost=lambda s : len(s), lists=l)
+
+    print(path)
+    print(cost)
+    
+    
+    #st = State(l[0])
+    #print(st)
+    #dict1={}
+    #dict1[st]='hello there'
+    #print(dict1)
+
---
+++ lab01_set-covering/utilities.py
@@ -0,0 +1,78 @@
+from typing import Callable
+from gx_utils import *
+import logging
+
+class State:
+    def __init__(self, data: set):
+        self._data = set(data)
+        #self._data = data.copy()
+
+    def __hash__(self):
+        return hash(bytes(self._data))
+
+    def __eq__(self, other):
+        return bytes(self._data) == bytes(other._data)
+
+    def __lt__(self, other):
+        return bytes(self._data) < bytes(other._data)
+
+    def __str__(self):
+        return str(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+    @property
+    def data(self):
+        return self._data
+
+    def copy_data(self):
+        return set(self._data)
+
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+def search(
+    initial_state,
+    goal_test,
+    parent_state,
+    state_cost,
+    priority_function,
+    unit_cost,
+    lists
+):
+    frontier = PriorityQueue()
+    parent_state.clear()
+    state_cost.clear()
+
+    state = initial_state
+    parent_state[state] = None
+    state_cost[state] = 0
+
+    while state is not None and not goal_test(state):
+        for a in lists:
+            new_state = State(state.data.union(a))
+            cost = unit_cost(a)
+            if new_state not in state_cost and new_state not in frontier:
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                frontier.push(new_state, p=priority_function(new_state))
+                #logging.debug(f"Added new node to frontier (cost={state_cost[new_state]})")
+            elif new_state in frontier and state_cost[new_state] > state_cost[state] + cost:
+                old_cost = state_cost[new_state]
+                parent_state[new_state] = state
+                state_cost[new_state] = state_cost[state] + cost
+                #logging.debug(f"Updated node cost in frontier: {old_cost} -> {state_cost[new_state]}")
+        if frontier:
+            state = frontier.pop()
+        else:
+            state = None
+
+    path = list()
+    s = state
+    while s:
+        path.append(s.copy_data())
+        s = parent_state[s]
+
+    logging.info(f"Found a solution in {len(path):,} steps; visited {len(state_cost):,} states")
+    return list(reversed(path)), state_cost[state]
