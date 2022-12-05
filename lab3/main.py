797e6b781335157684c1a8fc35fcdb330e09fb1c
Sergiu Abed s295149 <s295149@studenti.polito.it>
2022-12-05 13:00:34 +0100

    Removed comments from main

--- lab3/main.py
+++ lab3/main.py
@@ -2,19 +2,6 @@ from nim_utils import *
 from evolution import *
 
 if __name__ == '__main__':
-    #state = Nim(5)
-    #print(state)
-    #game_status = cook_status(state)
-    #print(f"active rows: {game_status['active_rows_number']}")
-    #shy_pick(state)
-    #print(state)
-    #game_status = cook_status(state)
-    #print(f"active rows: {game_status['active_rows_number']}")
-#
-    #shy_pick(state)
-    #print(state)
-    #game_status = cook_status(state)
-    #print(f"active rows: {game_status['active_rows_number']}")
 
     first_population = initialize_population(POPULATION_SIZE)
     best_individual = evolution(first_population)
